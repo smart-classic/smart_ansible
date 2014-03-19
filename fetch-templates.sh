904c04c4e06983ae1c942eda7a7cb9c1007cbed0
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2014-03-19 17:47:05 -0400

    Advance wget to full quiet

--- fetch-templates.sh
+++ fetch-templates.sh
@@ -6,8 +6,8 @@ branch=${2-v0.6.1}
 # Download configuration templates for SMART Servers
 mkdir $dir
 echo Downloading Configuration Templates for SMART Reference Container
-wget -nv https://raw.github.com/chb/smart_server/$branch/settings.py.default -O $dir/api_settings.py
-wget -nv https://raw.github.com/chb/smart_server/$branch/bootstrap_helpers/application_list.json.default -O $dir/application_list.json
-wget -nv https://raw.github.com/chb/smart_server/$branch/bootstrap_helpers/bootstrap_applications.py.default -O $dir/bootstrap_applications.py
-wget -nv https://raw.github.com/chb/smart_ui_server/$branch/settings.py.default -O $dir/ui_settings.py
-wget -nv https://raw.github.com/chb/smart_sample_apps/$branch/settings.py.default -O $dir/apps_settings.py
+wget -q https://raw.github.com/chb/smart_server/$branch/settings.py.default -O $dir/api_settings.py
+wget -q https://raw.github.com/chb/smart_server/$branch/bootstrap_helpers/application_list.json.default -O $dir/application_list.json
+wget -q https://raw.github.com/chb/smart_server/$branch/bootstrap_helpers/bootstrap_applications.py.default -O $dir/bootstrap_applications.py
+wget -q https://raw.github.com/chb/smart_ui_server/$branch/settings.py.default -O $dir/ui_settings.py
+wget -q https://raw.github.com/chb/smart_sample_apps/$branch/settings.py.default -O $dir/apps_settings.py
