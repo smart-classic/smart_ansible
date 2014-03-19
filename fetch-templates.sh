9840253b2bd73691fc308779fc2cc491bbdadce0
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2014-03-19 19:10:35 -0400

    Corrects API Verifier deployment

--- fetch-templates.sh
+++ fetch-templates.sh
@@ -11,3 +11,4 @@ wget -q https://raw.github.com/chb/smart_server/$branch/bootstrap_helpers/applic
 wget -q https://raw.github.com/chb/smart_server/$branch/bootstrap_helpers/bootstrap_applications.py.default -O $dir/bootstrap_applications.py
 wget -q https://raw.github.com/chb/smart_ui_server/$branch/settings.py.default -O $dir/ui_settings.py
 wget -q https://raw.github.com/chb/smart_sample_apps/$branch/settings.py.default -O $dir/apps_settings.py
+wget -q https://raw.github.com/chb/smart_sample_apps/$branch/api_verify/settings.py.default -O $dir/api_verify.py
