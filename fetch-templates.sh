9b1e65b5441f7d423d3ecc4773c8f53b9a3da5a1
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2014-03-19 16:20:01 -0400

    Set wget to not verbose mode for better output readability

--- fetch-templates.sh
+++ fetch-templates.sh
@@ -4,8 +4,9 @@ dir=${1-templates/config}
 
 # Download configuration templates for SMART Servers
 mkdir $dir
-wget https://raw.github.com/chb/smart_server/master/settings.py.default -O $dir/api_settings.py
-wget https://raw.github.com/chb/smart_server/master/bootstrap_helpers/application_list.json.default -O $dir/application_list.json
-wget https://raw.github.com/chb/smart_server/master/bootstrap_helpers/bootstrap_applications.py.default -O $dir/bootstrap_applications.py
-wget https://raw.github.com/chb/smart_ui_server/master/settings.py.default -O $dir/ui_settings.py
-wget https://raw.github.com/chb/smart_sample_apps/master/settings.py.default -O $dir/apps_settings.py
+echo Downloading Configuration Templates for SMART Reference Container
+wget -nv https://raw.github.com/chb/smart_server/master/settings.py.default -O $dir/api_settings.py
+wget -nv https://raw.github.com/chb/smart_server/master/bootstrap_helpers/application_list.json.default -O $dir/application_list.json
+wget -nv https://raw.github.com/chb/smart_server/master/bootstrap_helpers/bootstrap_applications.py.default -O $dir/bootstrap_applications.py
+wget -nv https://raw.github.com/chb/smart_ui_server/master/settings.py.default -O $dir/ui_settings.py
+wget -nv https://raw.github.com/chb/smart_sample_apps/master/settings.py.default -O $dir/apps_settings.py
