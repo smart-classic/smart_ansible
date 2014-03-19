b0e02bb89ac7c8c15b98ab31734946b2797643a2
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2014-03-19 16:47:23 -0400

    Adds version control for SMART

--- fetch-templates.sh
+++ fetch-templates.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 dir=${1-templates/config}
+branch=${2-v0.6.1}
 
 # Download configuration templates for SMART Servers
 mkdir $dir
 echo Downloading Configuration Templates for SMART Reference Container
-wget -nv https://raw.github.com/chb/smart_server/master/settings.py.default -O $dir/api_settings.py
-wget -nv https://raw.github.com/chb/smart_server/master/bootstrap_helpers/application_list.json.default -O $dir/application_list.json
-wget -nv https://raw.github.com/chb/smart_server/master/bootstrap_helpers/bootstrap_applications.py.default -O $dir/bootstrap_applications.py
-wget -nv https://raw.github.com/chb/smart_ui_server/master/settings.py.default -O $dir/ui_settings.py
-wget -nv https://raw.github.com/chb/smart_sample_apps/master/settings.py.default -O $dir/apps_settings.py
+wget -nv https://raw.github.com/chb/smart_server/$branch/settings.py.default -O $dir/api_settings.py
+wget -nv https://raw.github.com/chb/smart_server/$branch/bootstrap_helpers/application_list.json.default -O $dir/application_list.json
+wget -nv https://raw.github.com/chb/smart_server/$branch/bootstrap_helpers/bootstrap_applications.py.default -O $dir/bootstrap_applications.py
+wget -nv https://raw.github.com/chb/smart_ui_server/$branch/settings.py.default -O $dir/ui_settings.py
+wget -nv https://raw.github.com/chb/smart_sample_apps/$branch/settings.py.default -O $dir/apps_settings.py
